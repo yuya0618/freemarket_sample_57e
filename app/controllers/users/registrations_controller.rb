ba284653c7f49af811578cd956f50df2506b46ce
okamokokorooo <kokoroooo0607@gmail.com>
2019-08-25 17:27:23 +0900

    create

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  def index
+  end
 
   # GET /resource/sign_up
   # def new
@@ -59,4 +59,5 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :items
-  resources :users
+  resources :users, only: [:index, :edit, :update, :create]
+  devise_scope :user do
+    get    'signup',                to: 'users/registrations#index'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
