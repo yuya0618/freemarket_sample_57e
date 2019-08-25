c210e993519460f4d0d25f4c63908bc6bea90fb4
yuya0618 <yuya.aki.0618@gmail.com>
2019-08-25 14:36:32 +0900

    deploy

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,13 @@
-#サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-app_path = File.expand_path('../../', __FILE__)
+app_path = File.expand_path('../../../', __FILE__)
 
-#アプリケーションサーバの性能を決定する
 worker_processes 1
 
+working_directory "#{app_path}/current"
+
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 #アプリケーションの設置されているディレクトリを指定
 working_directory app_path
 
@@ -11,7 +15,7 @@ working_directory app_path
 pid "#{app_path}/tmp/pids/unicorn.pid"
 
 #ポート番号を指定
-listen "#{app_path}/tmp/sockets/unicorn.sock"
+listen 3000
 
 #エラーのログを記録するファイルを指定
 stderr_path "#{app_path}/log/unicorn.stderr.log"
