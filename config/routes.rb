eb32d250b73d52aac8313d23dddda3c4c20b40ce
okamokokorooo <kokoroooo0607@gmail.com>
2019-08-27 14:53:36 +0900

    aタグからlink_toへ

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     get    'signup',                to: 'users/registrations#index'
     get    'signup/registration',   to: 'users/registrations#new'
     get    'signup/sms_confirmation',      to: 'users/confirmations#new'
+    get    'signup/sms_confirmation',      to: 'users/confirmations/sms#new'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
