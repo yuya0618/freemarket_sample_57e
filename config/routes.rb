634e0796c9f970d2eafca9905f6624dd1a2fdf9b
snarqqq <yttkym2357@gmail.com>
2019-08-28 14:22:37 +0900

    markup-user-logout-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  root to: 'users#edit'
+  root to: 'users#to_destroy'
   devise_for :users
 
   # root 'items#index'
   resources :items
-  resources :users, only: [:index, :edit, :update, :create]
+  resources :users, only: [:index, :edit, :update, :create, :to_destroy]
   devise_scope :user do
     get    'signup',                to: 'users/registrations#index'
     get    'signup/registration',   to: 'users/registrations#new'
     get    'signup/sms_confirmation',      to: 'users/confirmations#new'
     get    'signup/sms_confirmation/sms',      to: 'users/sms/confirmations#new'
+    get    'users/sign_out',        to: 'users#to_destroy'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
