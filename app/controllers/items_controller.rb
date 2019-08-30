e2995532ad086d0edbc6b871c53fe6d66194eac6
LindseySawa <20betternbetter21@gmail.com>
2019-08-30 09:48:56 +0900

    fixing

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,8 +10,8 @@ class ItemsController < ApplicationController
   # GET /items/1
   # GET /items/1.json
   def show
-    # @next = Item.where("id > ?", id).order("id DESC")
-    # @previous = Item.where("id < ?", id).order("id ASC")
+    # @next = Item.where("id > ?", id).order("id DESC").first
+    # @previous = Item.where("id < ?", id).order("id ASC").first
   end
 
   # GET /items/new
