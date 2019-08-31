d9f008246a3b9565c63f7c3fbd42493be188bb9d
okamokokorooo <kokoroooo0607@gmail.com>
2019-08-31 18:20:51 +0900

    create

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,16 +2,28 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
     # GET /resource/sign_up
-    before_action :step1, only: [:step2,:step4,:step5]
-    before_action :step2, only: :step4
-    before_action :step4, only: :step5
+    # before_action :step1, only:[:step2,:step4,:step5]
+    # before_action :step2, only: [:step4,:step5]
+    # before_action :step4, only: [:step5]
   def step1
     @user = User.new
+    # binding.pry
   end
 
   def step2
-    session[:user_params] = user_params
-    @user = User.new(user_params)
+    session[:nickname] = user_params[:nickname]
+    session[:email] = user_params[:email]
+    session[:password] = user_params[:password]
+    session[:password_confirmation] = user_params[:password_confirmation]
+    session[:firstname] = user_params[:firstname]
+    session[:firstname_kana] = user_params[:firstname_kana]
+    session[:lastname] = user_params[:lastname]
+    session[:lastname_kana] = user_params[:lastname_kana]
+    session[:birth_year] = user_params[:birth_year]
+    session[:birth_month] = user_params[:birth_month]
+    session[:birth_day] = user_params[:birth_day]
+    @user = User.new
+
     # binding.pry
   end
 
@@ -19,23 +31,20 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def step4
-    session[:user_params].new(user_params[:phone_number])
-    @user = User.new(user_params)
+    session[:phone_number] = user_params[:phone_number]
+    @user = User.new
     @user.build_address
-    binding.pry
   end
 
   def step5
     # binding.pry
     session[:address_attributes] = user_params[:address_attributes]
-    @user = User.new(session[:user_params])
+    @user = User.new
     @user.build_address(session[:address_attributes])
     # binding.pry
   end
-  session = []
   def create
-    @user = User.new(session[:user_params])
-    @user.build_address(session[:address_attributes])
+    @user = User.new(nickname:session[:nickname], email: session[:email], password: session[:password], password_confirmation: session[:password_confirmation], lastname: session[:lastname],lastname_kana: session[:lastname_kana], firstname: session[:firstname], firstname_kana: session[:firstname_kana], birth_year: session[:birth_year], birth_month: session[:birth_month], birth_day: session[:birth_day], phone_number: session[:phone_number], address_attributes: session[:address_attributes])
     if @user.save
       # ログインするための情報を保管
       session[:id] = @user.id
@@ -43,7 +52,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
   private
   def user_params
-    params.require(:user).permit(:id, :nickname, :email, :password, :password_confirmation, :lastname,:lastnmae_kana, :firstname, :first_name_kana, :birthdate, :phone_number, address_attributes: [:id, :zip_code, :address_city, :address_number, :building])
+    params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :lastname,:lastname_kana, :firstname, :firstname_kana, :birth_year,:birth_month, :birth_day, :phone_number, address_attributes: [:id, :zip_code, :address_city, :address_number, :building,:prefecture_id])
   end
 
   # def address_params
