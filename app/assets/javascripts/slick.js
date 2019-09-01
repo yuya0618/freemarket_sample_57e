10f1e52b7294651302ec53ce94f4bb8dc779ecab
LindseySawa <20betternbetter21@gmail.com>
2019-09-01 13:36:12 +0900

    fixing
    got an advise from mentor

--- app/assets/javascripts/slick.js
+++ app/assets/javascripts/slick.js
@@ -3,12 +3,10 @@ $(function () {
   $('.item__left__main').slick({
     arrows: false,
     asNavFor: '.item__left__sub1',
-    // draggable: false,
   });
   $('.item__left__sub1').slick({
     asNavFor: '.item__left__main',
     focusOnSelect: true,
     slidesToShow: 5,
-    // slidesToScroll: 1,
   });
 });
