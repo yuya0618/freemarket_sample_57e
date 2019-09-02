74ba10a185604c8b8db99267ea1324c3de5429d1
LindseySawa <20betternbetter21@gmail.com>
2019-09-02 20:32:56 +0900

    size

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,13 +1,15 @@
-class Category < ActiveHash::Base
+class Category < ApplicationRecord
   has_many :items
   has_many :categories_sizes
   has_many :sizes, through: :categories_sizes
   belongs_to :parent, class_name: 'Category', optional: true
   has_many :children, class_name: 'Category', foreign_key: 'parent_id'
+end
 
+class Category < ActiveHash::Base
 
   self.data = [
-    {id: 0, name: '--'},{id: 1, name: 'レディース'},{id: 2, name: 'メンズ'},
+    {id: '', name: '---'},{id: 1, name: 'レディース'},{id: 2, name: 'メンズ'},
     {id: 3, name: 'ベビー・キッズ'},{id: 4, name: 'インテリア・住まい・小物'},
     {id: 5, name: '本・音楽・ゲーム'},{id: 6, name: 'おもちゃ・ホビー・グッズ'},
     {id: 7, name: 'コスメ・香水・美容'},{id: 8, name: '家電・スマホ・カメラ'},
@@ -15,5 +17,5 @@ class Category < ActiveHash::Base
     {id: 11, name: 'チケット'},{id: 12, name: '自動車・オートバイ'},
     {id: 12, name: 'その他'}
   ]
-  end
+
 end
--- app/models/size.rb
+++ app/models/size.rb
@@ -4,3 +4,15 @@ class Size < ApplicationRecord
   has_many :categories, through: :categories_sizes
 
 end
+
+class Category < ActiveHash::Base
+
+  self.data = [
+    {id: '', name: '---'},
+    {id: 1, name: 'XXS以下'},{id: 2, name: 'XS(SS)'},
+    {id: 3, name: 'S'},{id: 4, name: 'M'},
+    {id: 5, name: 'L'},{id: 6, name: 'XL(LL)'},
+    {id: 7, name: '2XL(3L)'},{id: 8, name: '3XL(4L)'},
+    {id: 9, name: '4XL(5L)以上'},{id: 10, name: 'FREESIZE'}
+  ]
+end
