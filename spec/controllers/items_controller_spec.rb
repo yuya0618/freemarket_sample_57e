519af39c79d2528cad22ef97044c2e67100c8667
LindseySawa <20betternbetter21@gmail.com>
2019-09-12 20:54:56 +0900

    adding

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,55 +1,55 @@
-# require 'rails_helper'
+require 'rails_helper'
 
 describe ItemsController, type: :controller do
 
   describe 'GET #index' do
+    #OK
     it "renders the :index template" do
       get :index
       expect(response).to render_template :index
     end
-
+    #OK
     it "returns a 200 response" do
       get :index
       expect(response).to have_http_status "200"
     end
-
   end
+
+
+
 
   describe 'GET #show' do
-    before do
-      # @user = FactoryBot.create(:user)
-      @item = FactoryBot.create(:item)
-    end
 
-    
-    # it "renders the :show template" do
-    #   get :show, params: {  id: 1 }
-    #   # expect(response).to render_template :show
+    # before { get :show, params: { id: item.id }, session: {} }
+    # it 'renders the :show template' do
+    #   expect(response).to render_template :show
     # end
-
-    it "responds successfully" do
-      # sign_in @user
-      get :show,params:{id:1}
-      expect(response).to be_success
-    end
+  #   let(:item) { create(:item) }
 
 
-  end
+  #   it "renders the :show template" do
+  #     get :show, params:{id:item.id }
+  #     expect(response).to render_template :show
+  #   end
+  # end
+
+
 
   describe 'GET #new' do
-    # it "renders the :new template" do
-    #   get :new
-    #   expect(response).to render_template :new
-    # end
+
+    #OK
+    it "renders the :new template" do
+      get :new
+      expect(response).to render_template :new
+    end
+
+    it "returns a 200 response" do
+      get :new
+      expect(response).to have_http_status "200"
+    end
+
 
   end
-
-
-
-
-
-
-
 
 
 
@@ -85,9 +85,27 @@ end
     #   get :show,params: {id: item.id}
     #   expect(response).to be_success
     # end
+        # it "renders the :show template" do
+    #   get :show, params: {  id: 2 }
+    #   expect(response).to render_template :show
+    # end
 
     # it "renders the :show template" do
     #   item = create(:item)
     #   get :show, id: item
     #   expect(response).to render_template :show
     # end
+
+
+    # before do
+    #   @user = FactoryBot.create(:user)
+    #   @item = FactoryBot.create(:item)
+    # end
+
+    
+    # it "responds successfully" do
+    #   # sign_in @user
+    #   get :show,params:{id:2}
+    #   expect(response).to be_success
+    # end
+
