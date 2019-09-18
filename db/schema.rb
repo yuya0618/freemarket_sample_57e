85c7dc390b3d41f99d362b02a6b69d2ad86179e4
snarqqq <yttkym2357@gmail.com>
2019-09-18 14:22:03 +0900

    delete-empty-line

--- db/schema.rb
+++ db/schema.rb
@@ -101,6 +101,7 @@ ActiveRecord::Schema.define(version: 2019_09_13_115021) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -114,10 +115,8 @@ ActiveRecord::Schema.define(version: 2019_09_13_115021) do
     t.integer "birth_year"
     t.integer "birth_month", null: false
     t.integer "birth_day", null: false
-    t.string "encrypted_password"
     t.string "phone_number", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["encrypted_password"], name: "index_users_on_encrypted_password", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
